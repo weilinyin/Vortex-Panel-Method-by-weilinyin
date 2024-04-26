fb5b8f97cf774bb9ce944ec2956c84af53ece203
weilinyin <2789682901@qq.com>
2024-04-27 00:26:21 +0800

    绘图优化

--- main.m
+++ main.m
@@ -1,13 +1,14 @@
 %计算主程序
 tic;
-alpha=5;
+alpha=[-8:15];
 s=1000;
 c=5;
-[cp,l,cl,x]=vortex_panel_method(0012,alpha,c,s,10);
+[cp,l,cl,x]=vortex_panel_method(2412,alpha,c,s,10);
 %plot(alpha,cl);
 
-plot(x(1:s),cp(1:s));
-hold;
-plot(x(s+1:end),cp(s+1:end));
+[X,Y]=meshgrid(alpha,x);
+s=surf(X,Y,cp,'FaceAlpha',0.5);
+s.EdgeColor= 'none';
+
 
 toc;
